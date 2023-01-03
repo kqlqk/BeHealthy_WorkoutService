58461bc28090d7dc95307d33066c28b890bde9cf
kqlqk <kononchuk99999@gmail.com>
2023-01-03 11:29:18 +0100

    repository layer
    * was added UserWorkoutRepository.java

---
+++ src/main/java/me/kqlqk/behealthy/workout_service/repository/UserWorkoutRepository.java
@@ -0,0 +1,12 @@
+package me.kqlqk.behealthy.workout_service.repository;
+
+import me.kqlqk.behealthy.workout_service.model.UserWorkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserWorkoutRepository extends JpaRepository<UserWorkout, Long> {
+    List<UserWorkout> getByUserId(long userId);
+}
